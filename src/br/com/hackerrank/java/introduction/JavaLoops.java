8d5a16ae8e7c7f25be2b188b61a1ddfb9a055b1a
Marcos Note <marcos@marcoscarvalho.com>
2016-06-29 06:47:39 -0300

    introduction

---
+++ src/br/com/hackerrank/java/introduction/JavaLoops.java
@@ -0,0 +1,51 @@
+package br.com.hackerrank.java.introduction;
+
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class JavaLoops {
+
+	private InputStream systemIn;
+
+	public JavaLoops() {
+		this(System.in);
+	}
+
+	public JavaLoops(InputStream in) {
+		this.systemIn = in;
+	}
+
+	public static void main(String[] args) {
+		new JavaLoops().execute();
+	}
+	
+	public void execute() {
+		Scanner sc = new Scanner(systemIn);
+		int tamanho = sc.nextInt();
+		for (int i = 0; i < tamanho; i++) {
+			
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			int n = sc.nextInt();
+			
+			for (int j = 1; j <= n; j++) {
+				int soma = 0;
+				
+				for (int k = 0; k < j; k++) {
+					soma += b * Math.pow(2, k);
+				}
+				
+				soma += a;
+				if(j == 1)
+					System.out.print(soma);
+				else
+					System.out.print(" " + soma);
+			}
+			System.out.println();
+		}
+		
+		sc.close();
+		
+	}
+
+}
